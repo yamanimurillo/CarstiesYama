47aecf2fd0129b5d90dbf896517e132e187a08ec
Yamani Murillo <yamani.murillo@gmail.com>
2023-08-09 22:06:43 -0600

    Initial commit

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AuctionService.Entities;
+
+namespace AuctionService.Data;
+
+public class AuctionDbContext: DbContext
+{
+    public AuctionDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Auction> Auctions { get; set; }
+}
---
+++ src/AuctionService/Data/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using AuctionService.Data.Seeds;
+using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class DbInitializer
+{
+    public static void InitDb(WebApplication app) 
+    {
+        using var scope = app.Services.CreateScope();
+
+        SeedData(scope.ServiceProvider.GetService<AuctionDbContext>());
+    }
+
+    private static void SeedData(AuctionDbContext context)
+    {
+        context.Database.Migrate();
+
+        if(context.Auctions.Any()) 
+        {
+            Console.WriteLine("Data already loaded");
+            return;
+        }
+
+        var auctions = Auctions.Seeds;
+
+        context.AddRange(auctions);
+
+        context.SaveChanges();
+    }
+}
